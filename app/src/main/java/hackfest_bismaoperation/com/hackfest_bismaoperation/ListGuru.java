1b0e9844e9514aeb20e36e35c051abdbf07326c7
Ryan Bagus Susilo <ryan2009bs@gmail.com>
2017-05-12 08:45:43 +0700

    first commit layout login dan register

--- app/src/main/java/hackfest_bismaoperation/com/hackfest_bismaoperation/ListGuru.java
+++ app/src/main/java/hackfest_bismaoperation/com/hackfest_bismaoperation/ListGuru.java
@@ -3,12 +3,11 @@ package hackfest_bismaoperation.com.hackfest_bismaoperation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListGuru extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_listguru);
     }
 }
